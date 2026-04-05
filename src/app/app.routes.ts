02e5296cc74c87a81ad1c51e69ce24137b81b99d
1112520 <1112520@users.noreply.github.com>
2026-04-05 08:42:05 +0800

    feat: Part2 Day1 - Angular project + 5 components + routing + navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ManageComponent } from './manage/manage.component';
+import { SearchComponent } from './search/search.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'manage', component: ManageComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'privacy', component: PrivacyComponent },
+];
